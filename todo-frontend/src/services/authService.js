469900267deca3865df4de0ddd5ebc74c3294271
Shreekar Sharma <shreekarsharma4512@gmail.com>
2026-06-15 14:31:11 +0530

    Update API URL for authentication service

--- todo-frontend/src/services/authService.js
+++ todo-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://task-management-app-kqkz.onrender.com/api/auth";
 
 // Register User
 export const registerUser = async (userData) => {
